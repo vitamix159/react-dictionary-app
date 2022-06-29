523340b4f1a65f9369fc290814e17dde604eb2c4
vitamix159 <97109007+vitamix159@users.noreply.github.com>
2022-06-29 13:03:01 +0300

    Created initial file structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from "./logo.jpg";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} alt="logo" className="App-logo img-fluid" />
+      </header>
+    </div>
+  );
+}
+        

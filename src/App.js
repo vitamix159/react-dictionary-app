04119fa4b8678604c20dc9c9a021c5da08f508e3
vitamix159 <97109007+vitamix159@users.noreply.github.com>
2022-08-09 20:46:59 +0300

    Added changes to css files

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <a href='https://hopeful-mirzakhani-0b7b52.netlify.app/' target="_blank">
+          <a href='https://hopeful-mirzakhani-0b7b52.netlify.app/' target="_blank" rel="noreferrer">
           <img src={logo} alt="logo" className="App-logo img-fluid" />
           </a>
         </header>
         <main>
           <Dictionary defaultKeyword="Love"/>
         </main>
-        <footer className="App-footer">
+        <footer className="App-footer mb-4">
           Coded by SheCodes student Vitalija Ramonaitė<br />
-          You can find the code on <a href='https://github.com/vitamix159/react-dictionary-app' target="_blank">GitHub</a>
+          You can find the code on <a href='https://github.com/vitamix159/react-dictionary-app' target="_blank" rel="noreferrer">GitHub</a>
         </footer>
       </div>
     </div>
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
     console.log(props.phonetic)
@@ -7,8 +8,9 @@ export default function Phonetic(props) {
             <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
                 Listen
             </a>
-            <br />
+        <span className="text">
             {props.phonetic.text}
+        </span>
         </div>
     );
 }

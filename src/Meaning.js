7d44a2770366a4b28e91bcd819b2b383aa2c13ef
vitamix159 <97109007+vitamix159@users.noreply.github.com>
2022-08-15 13:46:23 +0300

    Made changes to Synonyms.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,18 +3,19 @@ import Synonyms from "./Synonyms";
 import "./Meaning.css";
 
 export default function Meaning(props) {
+    console.log(props)
     return (
-        <div className="Meaning text-start">
+        <div className="Meaning">
         <h3>{props.meaning.partOfSpeech}</h3>
         {props.meaning.definitions.map(function(definition, index) {
             return (
                 <div key={index}>
                     <div className="definition">{definition.definition}</div>
                     <div className="example">{definition.example}</div>
-                    <Synonyms synonyms={definition.synonyms} />
                 </div>
             );
         })}
+            <Synonyms synonyms={props.meaning.synonyms} />
         </div>
     );
 }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-export default function Synonyms(props) {
-    if (props.synonyms) {
-        return (
-                <ul className="Synonyms">
-                    {props.synonyms.map(function (synonym, index) {
-                        return <li key={index}>{synonym}</li>;
-                })}
-                </ul>
-        );
-    } else {
-        return null;
-    }
+ export default function Synonyms(props) {
+  console.log(props)
+   if (props.synonyms) {
+    return (
+      <ul className="Synonyms mt-3">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
+    );
+  } else {
+    return null;
+  }
 }

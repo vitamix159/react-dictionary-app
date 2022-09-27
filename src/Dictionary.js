6c0a1bc63ad4a59df54e064fd8d8f50a8575467b
vitamix159 <97109007+vitamix159@users.noreply.github.com>
2022-09-27 15:30:19 +0300

    Included photos into a dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,21 +3,29 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
-
+import Photos from "./Photos";
 
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
     
     function handleResponse(response) {
         setResults(response.data[0]);
+    }
+    function handlePexelsResponse (response) {
+        setPhotos(response.data.photos)
     }
 
     function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+         let pexelsApiKey = "563492ad6f917000010000012796202e50b84ddca69190979dc851a9";
+         let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}`;
+         let headers = {"Authorization" : `Bearer ${pexelsApiKey}`};
+         axios.get(pexelsApiUrl,{ headers: headers }).then(handlePexelsResponse);
     }
 
 
@@ -54,6 +62,7 @@ export default function Dictionary(props) {
                     </form>
                 </section>
                 <Results results={results} />
+                <Photos photos={photos} />
             </div>
             );
     } else {
